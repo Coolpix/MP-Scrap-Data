1cdd04718e207f88f7cf0748ac45e970fb66fe5f
Sergio Alonso <sergio.ava@gmail.com>
2020-02-17 18:10:41 +0100

    Check

--- src/server.ts
+++ src/server.ts
@@ -1,23 +1,32 @@
-import App from './app'
+// import App from './app'
+import express from 'express'
 
-import * as bodyParser from 'body-parser'
-import cors from 'cors'
-import loggerMiddleware from './Middlewares/logger'
+// import * as bodyParser from 'body-parser'
+// import cors from 'cors'
+// import loggerMiddleware from './Middlewares/logger'
 
-import ApiController from './Controllers/api.controller'
-import ScraperController from './Controllers/scraper.controller'
-import { EventRepository } from './Repositories/EventRepository'
+// import ApiController from './Controllers/api.controller'
+// import ScraperController from './Controllers/scraper.controller'
+// import { EventRepository } from './Repositories/EventRepository'
 import Logger from "./Utils/logger";
 
-const credentials = process.env.PATH_SERVICE_ACCOUNT || './madridpatina-cefb9-firebase-adminsdk.json';
+// const credentials = process.env.PATH_SERVICE_ACCOUNT || './madridpatina-cefb9-firebase-adminsdk.json';
 const port: string | number = process.env.PORT || 8080;
 const defaultLogLevel = process.env.DEFAULT_LOG_LEVEL || 'info';
 
 const logger = new Logger(defaultLogLevel);
-const serviceAccount = require(credentials);
-const eventRepository = new EventRepository(serviceAccount);
+// const serviceAccount = require(credentials);
+// const eventRepository = new EventRepository(serviceAccount);
 
-const app = new App({
+const app = express();
+const router = express.Router()
+
+app.listen(port, () => {
+    logger.info(`App listening on the http://localhost:${port}`)
+ })
+
+
+/*const app = new App({
     port,
     logger,
     controllers: [
@@ -30,8 +39,8 @@ const app = new App({
         cors(),
         loggerMiddleware(logger)
     ]
-});
+});*/
 
-app.listen()
+// app.listen()
 
 // https://github.com/aligoren/express-typescript-test
