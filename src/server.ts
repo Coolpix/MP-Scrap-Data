c9e50076b3c5b8592bac72265315326486ca9766
Sergio Alonso <sergio.ava@gmail.com>
2020-02-17 19:03:45 +0100

    Check

--- src/server.ts
+++ src/server.ts
@@ -5,8 +5,8 @@ import cors from 'cors'
 import loggerMiddleware from './Middlewares/logger'
 
 //import ApiController from './Controllers/api.controller'
-import ScraperController from './Controllers/scraper.controller'
-import { EventRepository } from './Repositories/EventRepository'
+//import ScraperController from './Controllers/scraper.controller'
+//import { EventRepository } from './Repositories/EventRepository'
 import Logger from "./Utils/logger";
 
 const credentials = process.env.PATH_SERVICE_ACCOUNT || './madridpatina-cefb9-firebase-adminsdk.json';
@@ -15,14 +15,14 @@ const defaultLogLevel = process.env.DEFAULT_LOG_LEVEL || 'info';
 
 const logger = new Logger(defaultLogLevel);
 const serviceAccount = require(credentials);
-const eventRepository = new EventRepository(serviceAccount);
+//const eventRepository = new EventRepository(serviceAccount);
 
 const app = new App({
     port,
     logger,
     controllers: [
         //new ApiController(eventRepository),
-        new ScraperController(eventRepository)
+        //new ScraperController(eventRepository)
     ],
     middleWares: [
         bodyParser.json(),
