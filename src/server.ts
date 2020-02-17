a0783cdb3e9d09839e6a9ceb9ba1f623b7ba8543
Sergio Alonso <sergio.ava@gmail.com>
2020-02-17 19:19:05 +0100

    Check

--- src/server.ts
+++ src/server.ts
@@ -4,9 +4,9 @@ import * as bodyParser from 'body-parser'
 import cors from 'cors'
 import loggerMiddleware from './Middlewares/logger'
 
-//import ApiController from './Controllers/api.controller'
-//import ScraperController from './Controllers/scraper.controller'
-//import { EventRepository } from './Repositories/EventRepository'
+// import ApiController from './Controllers/api.controller'
+// import ScraperController from './Controllers/scraper.controller'
+// import { EventRepository } from './Repositories/EventRepository'
 import Logger from "./Utils/logger";
 
 const credentials = process.env.PATH_SERVICE_ACCOUNT || './madridpatina-cefb9-firebase-adminsdk.json';
@@ -15,9 +15,9 @@ const defaultLogLevel = process.env.DEFAULT_LOG_LEVEL || 'info';
 
 const logger = new Logger(defaultLogLevel);
 const serviceAccount = require(credentials);
-//const eventRepository = new EventRepository(serviceAccount);
+// const eventRepository = new EventRepository(serviceAccount);
 
-const app = new App({
+/*const app = new App({
     port,
     logger,
     controllers: [
@@ -30,8 +30,9 @@ const app = new App({
         cors(),
         loggerMiddleware(logger)
     ]
-});
+});*/
 
+const app = new App(8080, logger)
 app.listen()
 
 // https://github.com/aligoren/express-typescript-test
